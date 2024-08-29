01cff8ea3f1e45e6bcf9a8cb69058f8cffbda72a
greedjesse <67365952+greedjesse@users.noreply.github.com>
2024-08-29 17:05:17 +0800

    Make shadow works in editor.

--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -1,26 +1,37 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Shadow : MonoBehaviour
 {
     [SerializeField] private ShadowStats stats;
 
     private SpriteRenderer _spriteRenderer;
-    private Transform _parent;
-        
+    private GameObject _parent;
+    
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _parent = transform.parent;
+        _parent = transform.parent.gameObject;
     }
 
     void Start()
     {
+        _spriteRenderer.sprite = _parent.GetComponent<SpriteRenderer>().sprite;
         _spriteRenderer.color = stats.color;
     }
 
     void Update()
     {
-        transform.localRotation = _parent.transform.localRotation;
-        transform.position = _parent.position + new Vector3(stats.xOffset, stats.yOffset, 0);
+        HandleShadow();
     }
+
+    #region Shadow
+
+    private void HandleShadow()
+    {
+        transform.localRotation = _parent.transform.localRotation;
+        transform.position = _parent.transform.position + new Vector3(stats.xOffset, stats.yOffset, 0);
+    }
+
+    #endregion
 }
