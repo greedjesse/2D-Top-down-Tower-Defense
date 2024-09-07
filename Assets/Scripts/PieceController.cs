03e005aa1b49acb673c5717898ec6bbb9789e356
greedjesse <67365952+greedjesse@users.noreply.github.com>
2024-09-07 14:59:10 +0800

    Add move buffer.
    Early grounding system is also added to make the overall move buffer performance works smoother.

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -30,16 +30,21 @@ public class PieceController : MonoBehaviour
     #region Inputs
 
     private Inputs _inputs;
+
+    private float _timeLeftMouseWasPressed;
     
     private void GatherInputs()
     {
+        // Gather inputs.
         _inputs = new Inputs()
         {
             LeftMouseDown = Input.GetMouseButtonDown(0)
         };
 
+        // Fed the received inputs back to the game.
         if (_inputs.LeftMouseDown)
         {
+            _timeLeftMouseWasPressed = _time;
             _possibleDestination = _camera.ScreenToWorldPoint(Input.mousePosition);
             _moveToConsume = true;
         }
@@ -49,52 +54,53 @@ public class PieceController : MonoBehaviour
     
     void FixedUpdate()
     {
-        HandleGroundCheck();
-        
         HandleMovement();
     }
     
     #region Ground Check
 
-    [SerializeField] private bool _grounded;
+    private bool Grounded => !Moving || _time > _timeLastMoveExecuted + stats.moveTime - stats.earlyGroundingTimeOffset;
     
-    private void HandleGroundCheck()
-    {
-        
-    }
     #endregion
     
     #region Movement
     private float _speed;
     private Vector2 _velocity;
     
-    private Vector2 _source;
-    private Vector2 _destination;
+    [SerializeField] private Vector2 _source;
+    [SerializeField] private Vector2 _destination;
     private Vector2 _currentPos;
     private float _distanceToSource;
     public Vector2 shadowPos;
 
     private bool _moveToConsume;
+    private bool _moveBufferUsable;
 
     private Vector2 _possibleDestination;
     private float _distanceSD;
     private float _timeLastMoveExecuted;
 
-    private bool Moving => _time < _timeLastMoveExecuted + stats.moveTime; 
+    private bool Moving => _time < _timeLastMoveExecuted + stats.moveTime;
+    private bool HaveBufferedMove => _moveBufferUsable && _time < _timeLeftMouseWasPressed + stats.moveBuffer;
+    
     
     private void HandleMovement()
     {
+        _moveBufferUsable = Moving;
+        
+        // Check grounded.
         if (!Moving)
         {
-            _grounded = true;
             _source = _destination;
             shadowPos = _destination;
             _currentPos = _destination;
         }
-        
-        if (_grounded && _moveToConsume) ExecuteMove();
+
+        // Execute move?
+        if (Grounded && (_moveToConsume || HaveBufferedMove)) ExecuteMove();
         _moveToConsume = false;
         
+        // The actual movement.
         if (Moving)
         {
             _speed = Vector2.Distance(shadowPos, _destination) / stats.speedFactor;
@@ -111,11 +117,12 @@ public class PieceController : MonoBehaviour
     private void ExecuteMove()
     {
         _timeLastMoveExecuted = _time;
+        _source = shadowPos;
         _destination = _possibleDestination;
         _distanceSD = Vector2.Distance(_source, _destination);
-        _grounded = false;
     }
     
+    // Used for the y offset.
     private float Offset(float x, float max, float end)
     {
         if (_distanceSD == 0) return 0;
@@ -135,6 +142,7 @@ public class PieceController : MonoBehaviour
         return Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2);        
     }
 
+    // Holds the inputs.
     struct Inputs
     {
         public bool LeftMouseDown;
--- Assets/Scripts/ScriptableObjects/PieceStats.cs
+++ Assets/Scripts/ScriptableObjects/PieceStats.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class PieceStats : ScriptableObject
 {
     [Header("Movement")]
-    public float speedFactor = 6f;
+    public float speedFactor = 8f;
     public float moveTime = 0.8f;
+    public float moveBuffer = 0.1f;
     public float maxYOffset = 0.5f;
 
     [Header("Grounding")] 
-    public float groundingThreshold = 0.05f;
+    public float earlyGroundingTimeOffset = 0.05f;
 }
