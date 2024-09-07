508ab06d6039905d9b30c7e60771c12d1b8a58c2
greedjesse <67365952+greedjesse@users.noreply.github.com>
2024-09-07 10:36:58 +0800

    Pieces move based on mouse clicks.
    What's next: Add move buffer.

--- Assets/Scripts/PieceController.cs
+++ Assets/Scripts/PieceController.cs
@@ -1,13 +1,69 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class PieceController : MonoBehaviour
 {
+    #region Components
+
     [SerializeField] private PieceStats stats;
+    private Camera _camera;
+
+    #endregion
+    
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+    
+    void Update() 
+    {
+        GatherInputs();
+    }
+
+    #region Inputs
+
+    private Inputs _inputs;
+    
+    private void GatherInputs()
+    {
+        _inputs = new Inputs()
+        {
+            LeftMouseDown = Input.GetMouseButtonDown(0)
+        };
+
+        if (_inputs.LeftMouseDown)
+        {
+            _possibleDestination = _camera.ScreenToWorldPoint(Input.mousePosition);
+            _moveToConsume = true;
+        }
+    }
+
+    #endregion
     
     void FixedUpdate()
     {
+        HandleGroundCheck();
+        
         HandleMovement();
     }
+    
+    #region Ground Check
+
+    [SerializeField] private bool _grounded;
+    
+    private void HandleGroundCheck()
+    {
+        _grounded = false;
+        Debug.Log(SquaredDist(shadowPos, _destination));
+        if (SquaredDist(shadowPos, _destination) < stats.groundingThreshold)
+        {
+            _grounded = true;
+            _source = _destination;
+            shadowPos = _destination;
+            _currentPos = _destination;
+        }
+    }
+    #endregion
     
     #region Movement
     [SerializeField] private Vector2 _source;
@@ -15,6 +71,9 @@ public class PieceController : MonoBehaviour
     [SerializeField] private Vector2 _currentPos;
     public Vector2 shadowPos;
 
+    private bool _moveToConsume;
+    private Vector2 _possibleDestination;
+    
     [SerializeField] private float _distanceSD;  // TODO -> Remember to change this after determined the new source.
 
     private float _speed;
@@ -22,8 +81,13 @@ public class PieceController : MonoBehaviour
     
     private void HandleMovement()
     {
-        // TODO -> Remove.
-        _distanceSD = Vector2.Distance(_source, _destination);
+        if (_grounded && _moveToConsume)
+        {
+            _destination = _possibleDestination;
+            _distanceSD = Vector2.Distance(_source, _destination);
+            _grounded = false;
+        }
+        _moveToConsume = false;
         
         _speed = Vector2.Distance(shadowPos, _destination) / stats.speedFactor;
         _velocity = (_destination - _source).normalized * _speed;
@@ -51,6 +115,11 @@ public class PieceController : MonoBehaviour
 
     private float SquaredDist(Vector2 a, Vector2 b)
     {
-        return Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y * b.y, 2);        
+        return Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2);        
+    }
+
+    struct Inputs
+    {
+        public bool LeftMouseDown;
     }
 }
--- Assets/Scripts/ScriptableObjects/PieceStats.cs
+++ Assets/Scripts/ScriptableObjects/PieceStats.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu]
 public class PieceStats : ScriptableObject
 {
+    [Header("Movement")]
     public float speedFactor = 6f;
     public float maxYOffset = 0.5f;
+
+    [Header("Grounding")] 
+    public float groundingThreshold = 0.05f;
 }
